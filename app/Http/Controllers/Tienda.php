0007d50c54a28bc13f240f511def2c0fc9e11ff1
SoulHarry <hvelez56@gmail.com>
2020-05-13 13:22:11 -0500

    Laravel 7 CRUD

---
+++ app/Http/Controllers/Tienda.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tienda;
+use Illuminate\Http\Request;
+
+class TiendaController extends Controller
+{
+    //
+    public function index(){
+        
+    }
+}
